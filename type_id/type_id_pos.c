7b650eab9877c7162f3e2eb602f7d45a407d9c01
Mahaut Latinis <malatini@student.42.fr>
2021-03-22 11:50:57 +0100

    164 ok

--- type_id/type_id_pos.c
+++ type_id/type_id_pos.c
@@ -6,7 +6,7 @@
 /*   By: malatini <malatini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/15 18:09:32 by malatini          #+#    #+#             */
-/*   Updated: 2021/03/22 11:38:51 by malatini         ###   ########.fr       */
+/*   Updated: 2021/03/22 11:50:02 by malatini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@ int		width_precision_pos(t_format *f, int n, char c)
 		w_to_print = f->width - p_to_print - n_size_i(n);
 	else
 		w_to_print = f->width - n_size_i(n);
-	if (f->precision < 0 && f->width > 0)
+	if (f->precision <= 0 && f->width > 0)
 	{
+
 		w_to_print = n == 0 ? f->width : f->width - n_size_i(n);
 		p_to_print = 0;
 	}
@@ -33,9 +34,9 @@ int		width_precision_pos(t_format *f, int n, char c)
 		c = ' ';
 	if (n > 10)
 		i += print_x_time(c, w_to_print);
-	else if (n < 10 && n > 0 && f->flags.zero_pad == true)
+	else if (n < 10 && n > 0 && f->flags.zero_pad == true && f->precision != 0)
 		i += print_x_time('0', w_to_print);
-	else if (n < 10 && n > 0 && f->flags.zero_pad == false)
+	else if ((n < 10 && n > 0 && f->flags.zero_pad == false) || (n < 10 && n > 0 && f->flags.zero_pad == true && f->precision == 0))
 		i += print_x_time(' ', w_to_print);
 	i += print_x_time('0', p_to_print);
 	i += ft_putnbr_i(n);
